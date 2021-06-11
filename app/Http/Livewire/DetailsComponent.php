64367d41946568287b96e66f1906cd39e79642d8
konouz <konouzkrtoumeh@gmail.com>
2021-06-11 14:57:21 +0300

    product details

---
+++ app/Http/Livewire/DetailsComponent.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use app\Model\Product;
+use Livewire\Component;
+use Cart;
+
+class DetailsComponent extends Component
+{
+    public $slug;
+    public function mount($slug)
+    {
+
+        $this->slug =$slug;
+    }
+
+
+
+
+
+    public function render()
+    {
+        $product= Product::where('slug',$this->slug)->first();
+        return view('livewire.details-component',['product'->$product])->layout('layouts.base');
+    }
+        // public function store ($product_id,$product_name,$product_price)
+    // {
+
+    //     Cart::add($product_id,$product_name,$product_price)->associate('app\Models\Product');
+    //     session()->flash('success massage','Item added in Cart');
+    //     return redirect()->route('product.cart');
+    // }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 use App\Http\Livewire\HomeComponent;
 use App\Http\Livewire\ShopComponent;
 use App\Http\Livewire\CartComponent;
+use App\Http\Livewire\DetailsComponent;
 use App\Http\Livewire\checkoutComponent;
 use App\Models\Product;
 // use Database\Seeders\VoyagerDatabaseSeeder;
@@ -26,9 +27,9 @@ Route::resource('products', ProductController::class);
 Route::resource('categories', CategoryController::class);
 Route::resource('brands', BrandController::class);
 
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth'])->name('dashboard');
+ Route::get('/dashboard', function () {
+     return view('dashboard');
+ })->middleware(['auth'])->name('dashboard');
 
 
 
@@ -42,6 +43,8 @@ Route::get('/', HomeComponent::class );
 
 Route::get('/shop', ShopComponent::class );
 
-Route::get('/cart', CartComponent::class );
+Route::get('/cart', CartComponent::class )->name('product.cart');
 
 Route::get('/checkout', CheckoutComponent::class );
+
+Route::get('/product/(slug)', DetailsComponent::class)->name('product.details');
